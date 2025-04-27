98b48c206afcf1692d52b931c8ae9813f138c684
Shayo <Shayo@SHAYO>
2025-04-27 13:08:44 +0300

    trapping rain water(42).py

--- trapping rain water(42) in proccess.py
+++ trapping rain water(42) in proccess.py
@@ -1,53 +1,28 @@
 class Solution:
-    def get_first_tile(self, height):
-        indexFirstTile = 0
-        size = len(height)
-
-        while indexFirstTile < size and height[indexFirstTile] == 0:
-            indexFirstTile += 1
-        
-        if indexFirstTile == size:
-            return -1
-        return indexFirstTile
-
-    def check_if_close(self, height, index):
-        currIndex = index + 1
-        heightNeedToPass = height[index]
-        size = len(height)
-
-        while currIndex < size:
-            if height[currIndex] >= heightNeedToPass:
-                return True
-            currIndex += 1
-        
-        return False
-
-
     def trap(self, height):
-        sumWater = 0
-        indexFirstTile = self.get_first_tile(height)
+        leftMax = 0
+        rightMax = 0
         size = len(height)
-        isGoingToClose = False
-        #if all tiles are on the ground
-        if indexFirstTile == -1:
-            return 0
-        currTileIndex = indexFirstTile + 1
-        currLeftHeight = height[indexFirstTile]
-        while currTileIndex < size:
-            if height[currTileIndex] >= currLeftHeight:
-                indexFirstTile = currTileIndex
-                while indexFirstTile < size and not isGoingToClose:
-                    isGoingToClose = self.check_if_close(height,indexFirstTile)
-                    if not isGoingToClose:
-                        indexFirstTile += 1
-                if indexFirstTile < size:
-                    currLeftHeight = height[indexFirstTile]
-                currTileIndex = indexFirstTile + 1
-            else:
-                sumWater += currLeftHeight - height[currTileIndex]
-            currTileIndex += 1
+        leftIndex = 0
+        rightIndex = size - 1
+        sumWater = 0
 
+        while leftIndex < rightIndex:
+            if height[leftIndex] < height[rightIndex]:
+                if height[leftIndex] >= leftMax:
+                    leftMax = height[leftIndex]
+                else:
+                    sumWater += leftMax - height[leftIndex]
+                leftIndex += 1
+            else:
+                if height[rightIndex] >= rightMax:
+                    rightMax = height[rightIndex]
+                else:
+                    sumWater += rightMax - height[rightIndex]
+                rightIndex -= 1
+        
         return sumWater
+
             
         
 # Example usage:
