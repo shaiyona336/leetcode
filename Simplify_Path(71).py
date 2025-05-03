5c781d41d9327e019a0157101eb87a3365d45394
Shayo <Shayo@SHAYO>
2025-05-03 19:25:52 +0300

    simplify_path(71).py finished

--- Simplify_Path(71).py
+++ Simplify_Path(71).py
@@ -1,38 +1,47 @@
-def doubleDot(path): #if the next folder is .. need to remove last folder
-    words = newPath.split("")
-    newPath = ""
+class Solution:
+    def doubleDot(self,path):  # if the next folder is .. need to remove last folder
+        words = path.split("/")
+        newPath = "/"
 
-    for wordIndex in range(1,len(path)-1)
-        newPath += "/" + path[word]
+        for wordIndex in range(1, len(words) - 2):
+            currWord = words[wordIndex]
+            newPath += currWord + "/"
 
-    return newPath if newPath != "" else "/"
+        return newPath if newPath != "" else "/"
 
 
-def simplifyPath(path):
-  currIndex = 1
-  size = len(newPath)
-  newPath = '/'
-  if len(newPath) < 1 or path[0] != '/':
-    return newPath
-  isSlashMode = True
-  nextFolder = ""
-  while currIndex < size:
-    while isSlashMode and currIndex < size:
-      if path[currIndex] = '/':
-        currIndex += 1
-      else:
-        isSlashMode = False
-    while not isSlashMode and currIndex < size:
-        if path[currIndex] != '/':
-            nextFolder += newPath
+    def simplifyPath(self, path: str) -> str:
+        currIndex = 1
+        size = len(path)
+        newPath = "/"
+        if len(newPath) < 1 or path[0] != '/':
+            return newPath
+        isSlashMode = True
+        nextFolder = ""
+        while currIndex < size:
+            while isSlashMode and currIndex < size:
+                if path[currIndex] == '/':
+                    currIndex += 1
+                else:
+                    isSlashMode = False
+            while not isSlashMode and currIndex < size:
+                if path[currIndex] != '/':
+                    nextFolder += path[currIndex]
+                    currIndex += 1
+                else:
+                    isSlashMode = True
+            if nextFolder == ".":
+                pass
+            elif nextFolder == "..":
+                newPath = self.doubleDot(newPath)
+            else:
+                newPath += nextFolder + "/"
+
             currIndex += 1
-        else:
-            isSlashMode = True
-    if nextFolder == ".":
-        pass
-    elif nextFolder == "..":
-        doubleDot(newPath)
-    else:
-        newPath += nextFolder
+            nextFolder = ""
+        
+        while len(newPath) > 1 and newPath[-1] == "/":
+            newPath = newPath[:-1]
 
-    currIndex += 1
+        return newPath
+
