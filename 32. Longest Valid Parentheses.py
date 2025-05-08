b96da4c9a52ca460e521458057d8f85773b627dd
Shayo <Shayo@SHAYO>
2025-05-08 16:04:30 +0300

    s

--- 32. Longest Valid Parentheses.py
+++ 32. Longest Valid Parentheses.py
@@ -1,23 +1,16 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        max_len = 0
-        stack = [-1]  # holds index of last unmatched ')'
+        best = 0
+        stack = [-1]
         
-        for i, ch in enumerate(s):
-            if ch == '(':
+        for i, char in enumerate(s):
+            if char == '(':
                 stack.append(i)
             else:
-                # pop matching '(' or the sentinel
                 stack.pop()
-                if not stack:
-                    # no boundary to match from, so set current ')' as boundary
-                    stack.append(i)
+                if stack != []:
+                    best=max(best,i-stack[-1])
                 else:
-                    # valid substring from stack.top()+1 to i
-                    max_len = max(max_len, i - stack[-1])
-        return max_len
-    
-solution = Solution()
-s = "()"
-result = solution.longestValidParentheses(s)
-print(result)  # Expected output: 4 (the longest valid parentheses substring is "(()())")
+                    stack.append(i)
+
+        return best
